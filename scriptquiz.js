9a8465a7b391b0b5a7a5bb160869e8efb9bc8bb3
Abhinav Singh <117064205+abhinav26111@users.noreply.github.com>
2024-01-16 01:36:57 +0530

    Add files via upload

---
+++ scriptquiz.js
@@ -0,0 +1,120 @@
+const questions = [
+    {
+        question: "Which is the largest animal in the world?",
+        answers: [
+            { text: "Shark", correct: false },
+            { text: "Blue Whale", correct: true },
+            { text: "Crocodile", correct: false },
+            { text: "Elephant", correct: false },
+        ]
+    },
+    {
+        question: "Which is the largest bird in the world?",
+        answers: [
+            { text: "Albatross", correct: false },
+            { text: "Ostrich", correct: true },
+            { text: "Humming Bird", correct: false },
+            { text: "Emu", correct: false },
+        ]
+    },
+    {
+        question: "Which is the largest flower in the world?",
+        answers: [
+            { text: "Rafflesia", correct: true },
+            { text: "Rose", correct: false },
+            { text: "Lotus", correct: false },
+            { text: "Lily", correct: false },
+        ]
+    },
+    {
+        question: "Which is the smallest country(population) in the world?",
+        answers: [
+            { text: "Hungary", correct: false },
+            { text: "Kuwait", correct: false },
+            { text: "Vatican City", correct: true },
+            { text: "Saint Helena", correct: false },
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex]
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer)
+    });
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild)
+    }
+}
+
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    }
+    else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+});
+
+startQuiz();
